f5307013243b4f13c06f59dbc09319ba1d22da68
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2023-08-31 13:04:16 -0300

    Add files via upload

---
+++ exec5_pythonbrasil.py
@@ -0,0 +1,8 @@
+# 5. Faça um Programa que converta metros para centímetros.
+
+def main():
+    numero1 = float(input("Digite quantos metros você deseja converter: "))
+    numero2 = numero1 * 100
+    print(f'Convertemos em centimetros e o valor é: {numero2} centimetros.')
+main()
+    
